fb83e3d9ab627d97f819cd3f4fceed8b669a9093
Admin <Kkuji>
2023-03-13 19:24:09 +0300

    Base movements and right tilemap collider

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,6 +4,7 @@ public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
     private Vector2 _direction;
+    private Vector2 _directionLast;
     private PlayerMover _playerMover;
 
     private void Start()
@@ -16,8 +17,16 @@ public class PlayerAnimator : MonoBehaviour
     {
         _direction = _playerMover.Direction;
 
-        _animator.SetFloat("Horizontal", _direction.x);
-        _animator.SetFloat("Vertical", _direction.y);
-        _animator.SetFloat("Speed", _direction.magnitude);
+        if (_direction.magnitude != 0)
+            _directionLast = _direction;
+
+        SetAnimatorFloats(_directionLast.x, _directionLast.y, _direction.magnitude);
+    }
+
+    private void SetAnimatorFloats(float horizontal, float vertical, float speed)
+    {
+        _animator.SetFloat("Horizontal", horizontal);
+        _animator.SetFloat("Vertical", vertical);
+        _animator.SetFloat("Speed", speed);
     }
 }
